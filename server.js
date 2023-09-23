d2a05aa26c03028508388d8caa86f14598d0c0fd
Ricardo Scuotto <ucfscuotto@gmail.com>
2023-09-23 16:23:57 -0400

    added additional code to server.js

--- server.js
+++ server.js
@@ -18,3 +18,53 @@ connection.connect((err) => {
     // Start Application
     start();
 });
+
+// Function to begin employee tracker application
+function start() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "Please select the following options:",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "Exit",
+            ],
+        })
+
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+                case "Exit":
+                    connection.end();
+                    console.log("Take care!");
+                    break;
+            }
+        });
+}
